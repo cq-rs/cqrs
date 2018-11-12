a7271f51f3a27f3f2603e016080648cc8085f60d
Marcus Griep <mgriep@vistaprint.com>
2018-11-12 16:16:43 -0500

    Add some static assertions

--- cqrs-postgres/src/lib.rs
+++ cqrs-postgres/src/lib.rs
@@ -22,9 +22,35 @@ extern crate postgres;
 extern crate serde;
 extern crate serde_json;
 
+#[cfg(test)] extern crate cqrs;
+#[cfg(test)] extern crate cqrs_todo_core;
+#[cfg(test)] extern crate static_assertions;
+
 mod error;
 mod store;
 mod util;
 
 pub use store::PostgresStore;
 pub use error::{LoadError, PersistError};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cqrs_todo_core::TodoAggregate;
+    use static_assertions::assert_impl;
+
+    #[test]
+    fn postgres_store_is_an_entity_store() {
+        assert_impl!(PostgresStore<TodoAggregate>, cqrs::EntityStore<TodoAggregate>);
+    }
+
+    #[test]
+    fn postgres_store_is_an_entity_source() {
+        assert_impl!(PostgresStore<TodoAggregate>, cqrs::EntitySource<TodoAggregate>);
+    }
+
+    #[test]
+    fn postgres_store_is_an_entity_sink() {
+        assert_impl!(PostgresStore<TodoAggregate>, cqrs::EntitySink<TodoAggregate>);
+    }
+}
